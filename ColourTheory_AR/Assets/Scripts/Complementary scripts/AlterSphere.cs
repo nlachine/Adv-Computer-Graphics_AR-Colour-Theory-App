3bf81899b12f8ff93d2c07ca85a9196d86be54d4
NoahColangelo <noah.colangelo@uoit.net>
2021-02-25 15:32:04 -0500

    [Add] Started adding in the complementary color match, Has a seperate tracker, seperate materials, and has the scene set up, working on the functionality

---
+++ ColourTheory_AR/Assets/Scripts/Complementary scripts/AlterSphere.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlterSphere : MonoBehaviour
+{
+    public Slider redSlider, yellowSlider, blueSlider;
+
+    [SerializeField]
+    Material AlterMaterial;
+
+    private Color AlterColorRYB;
+    private Color AlterColorRGB;
+    private HandleColour _handleColour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AlterColorRYB = AlterMaterial.color;
+        AlterColorRYB = _handleColour.Randomize();
+        AlterColorRGB = _handleColour.ConvertToRGB(AlterColorRYB.r, AlterColorRYB.g, AlterColorRYB.b);
+        AlterMaterial.color = AlterColorRGB;
+
+        redSlider.value = AlterColorRYB.r;
+        yellowSlider.value = AlterColorRYB.g;
+        blueSlider.value = AlterColorRYB.b;
+    }
+
+    public void sliderUpdate()
+    {
+
+        //Save RYB values from slider
+        AlterColorRYB.r = redSlider.value;
+        AlterColorRYB.g = yellowSlider.value; //yellow
+        AlterColorRYB.b = blueSlider.value;
+
+        //Convert slider to RGB for display
+        AlterColorRGB = _handleColour.ConvertToRGB(AlterColorRYB.r, AlterColorRYB.g, AlterColorRYB.b);
+        AlterMaterial.color = AlterColorRGB;
+    }
+        // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ColourTheory_AR/Assets/Scripts/Complementary scripts/ResultSphere.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultSphere : MonoBehaviour
+{
+    [SerializeField]
+    GameObject AlterSphere;
+    [SerializeField]
+    GameObject ComplementarySphere;
+
+    [SerializeField]
+    Material AlterMat;
+    [SerializeField]
+    Material ComplementaryMat;
+    [SerializeField]
+    Material ResultMat;
+
+    private Color AlterColor;
+    private Color ComplementaryColor;
+    private Color ResultColor;
+
+    private HandleColour _handleColour;
+
+    float tolerance = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AlterColor = AlterMat.color;
+        ComplementaryColor = ComplementaryMat.color;
+        ResultColor = ResultMat.color;
+    }
+
+    void checkResult()
+    {
+        //if( ResultColor.r >= )
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ResultColor.r = (ComplementaryColor.r - 1.0f) + AlterColor.r;
+        ResultColor.g = (ComplementaryColor.g - 1.0f) + AlterColor.g;
+        ResultColor.b = (ComplementaryColor.b - 1.0f) + AlterColor.b;
+        ResultMat.color = ResultColor;
+    }
+}
