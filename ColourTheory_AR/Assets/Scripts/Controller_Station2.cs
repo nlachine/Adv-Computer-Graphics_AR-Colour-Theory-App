eb99cbab3f0bc485bffb877967e826bfde2c56cb
Noah Lachine <noah.lachine@ontariotechu.net>
2021-03-25 15:11:45 -0400

    [MOD] Separate flashing script

--- ColourTheory_AR/Assets/Scripts/Controller_Station2.cs
+++ ColourTheory_AR/Assets/Scripts/Controller_Station2.cs
@@ -3,7 +3,12 @@
 public class Controller_Station2 : MonoBehaviour
 {
 
-    //Colour Flashing Controls
+    //---- Utility Scripts ----//
+    [Header("Utility Scripts")]
+    MaterialAnimations materialAnimator;
+
+
+    //---- Colour Flashing Controls ----//
     [Header("Flashing Controls")]
     [Range(0.0f, 1.0f)]
     public float flashAmplitude = 0.5f;
@@ -14,7 +19,7 @@ public class Controller_Station2 : MonoBehaviour
 
 
 
-    //Declare Colour Wheel Game Objects and Mesh Renderers
+    //---- Declare Colour Wheel Game Objects and Mesh Renderers ----//
     public GameObject colourWheel;
     [Header("CW Pieces")]
     GameObject redPiece;
@@ -42,7 +47,7 @@ public class Controller_Station2 : MonoBehaviour
     GameObject redPurplePiece;
     Renderer redPurpleRenderer;
 
-    //Declare Colour Wheel Materials (Drag In)
+    //---- Declare Colour Wheel Materials (Drag In) ----//
     [Header("CW Materials")]
     public Material redMat;
     public Material redOrangeMat;
@@ -55,31 +60,34 @@ public class Controller_Station2 : MonoBehaviour
     public Material blueMat;
     public Material bluePurpleMat;
     public Material purpleMat;
-    public Material redPurpleMat;           
+    public Material redPurpleMat;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //Set Default Flashing Parameters
-         flashAmplitude = 0.5f;
-         flashOffset = 1f;
-         flashSpeed = 5;
+        //---- Get Utility Scripts ----//
+        materialAnimator = this.GetComponent<MaterialAnimations>();
 
-        //Set Colour Wheel Game Objects and Mesh Renderers
+        //---- Set Default Flashing Parameters ----//
+        flashAmplitude = 0.5f;
+        flashOffset = 1f;
+        flashSpeed = 5;
+
+        //---- Get Colour Wheel Game Objects and Mesh Renderers ----//
         redPiece = colourWheel.gameObject.transform.Find("1_Red").GetChild(0).gameObject;
-        redOrangePiece= colourWheel.gameObject.transform.Find("2_Red-Orange").GetChild(0).gameObject;
-        orangePiece= colourWheel.gameObject.transform.Find("3_Orange").GetChild(0).gameObject;
-        yellowOrangePiece= colourWheel.gameObject.transform.Find("4_Yellow-Orange").GetChild(0).gameObject;
-        yellowPiece= colourWheel.gameObject.transform.Find("5_Yellow").GetChild(0).gameObject;
-        yellowGreenPiece= colourWheel.gameObject.transform.Find("6_Yellow-Green").GetChild(0).gameObject;
-        greenPiece= colourWheel.gameObject.transform.Find("7_Green").GetChild(0).gameObject;
-        blueGreenPiece= colourWheel.gameObject.transform.Find("8_Blue-Green").GetChild(0).gameObject;
-        bluePiece= colourWheel.gameObject.transform.Find("9_Blue").GetChild(0).gameObject;
-        bluePurplePiece= colourWheel.gameObject.transform.Find("10_Blue-Purple").GetChild(0).gameObject;
-        purplePiece= colourWheel.gameObject.transform.Find("11_Purple").GetChild(0).gameObject;
-        redPurplePiece= colourWheel.gameObject.transform.Find("12_Red-Purple").GetChild(0).gameObject;
+        redOrangePiece = colourWheel.gameObject.transform.Find("2_Red-Orange").GetChild(0).gameObject;
+        orangePiece = colourWheel.gameObject.transform.Find("3_Orange").GetChild(0).gameObject;
+        yellowOrangePiece = colourWheel.gameObject.transform.Find("4_Yellow-Orange").GetChild(0).gameObject;
+        yellowPiece = colourWheel.gameObject.transform.Find("5_Yellow").GetChild(0).gameObject;
+        yellowGreenPiece = colourWheel.gameObject.transform.Find("6_Yellow-Green").GetChild(0).gameObject;
+        greenPiece = colourWheel.gameObject.transform.Find("7_Green").GetChild(0).gameObject;
+        blueGreenPiece = colourWheel.gameObject.transform.Find("8_Blue-Green").GetChild(0).gameObject;
+        bluePiece = colourWheel.gameObject.transform.Find("9_Blue").GetChild(0).gameObject;
+        bluePurplePiece = colourWheel.gameObject.transform.Find("10_Blue-Purple").GetChild(0).gameObject;
+        purplePiece = colourWheel.gameObject.transform.Find("11_Purple").GetChild(0).gameObject;
+        redPurplePiece = colourWheel.gameObject.transform.Find("12_Red-Purple").GetChild(0).gameObject;
 
         redRenderer = redPiece.GetComponent<MeshRenderer>();
         redOrangeRenderer = redOrangePiece.GetComponent<MeshRenderer>();
@@ -94,7 +102,7 @@ public class Controller_Station2 : MonoBehaviour
         purpleRenderer = purplePiece.GetComponent<MeshRenderer>();
         redPurpleRenderer = redPurplePiece.GetComponent<MeshRenderer>();
 
-        //Set Materials on Pieces
+        //---- Set Materials on Pieces ----//
         redRenderer.material = redMat;
         //redOrangeRenderer.material = redOrangeMat;
         //orangeRenderer.material = orangeMat;
@@ -109,19 +117,10 @@ public class Controller_Station2 : MonoBehaviour
         //redPurpleRenderer.material = redPurpleMat;
     }
 
-    void FlashingAlpha(Renderer renderer)
-    {
-        Color tempCol = renderer.material.color;
-        tempCol.a = flashAmplitude * Mathf.Sin(Time.time * flashSpeed) + flashOffset;
-        renderer.material.color = tempCol;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        FlashingAlpha(redRenderer);
-        FlashingAlpha(yellowRenderer);
-        FlashingAlpha(blueRenderer);
-        Debug.Log(redPiece.GetComponent<MeshRenderer>().material.color);
+        materialAnimator.FlashingAlpha(redRenderer, flashAmplitude, flashSpeed, flashOffset);
+        materialAnimator.FlashingAlpha(yellowRenderer, flashAmplitude, flashSpeed, flashOffset);
+        materialAnimator.FlashingAlpha(blueRenderer, flashAmplitude, flashSpeed, flashOffset);
     }
 }
