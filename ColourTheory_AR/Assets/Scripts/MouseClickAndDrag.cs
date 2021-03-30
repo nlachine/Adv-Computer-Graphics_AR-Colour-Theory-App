065951c8be5308a331dcaa23a9a799956e4574c4
NoahColangelo <noah.colangelo@uoit.net>
2021-03-29 23:05:21 -0400

    [ADD] added the controller script for station 1 with about 90% functionality

--- ColourTheory_AR/Assets/Scripts/MouseClickAndDrag.cs
+++ ColourTheory_AR/Assets/Scripts/MouseClickAndDrag.cs
@@ -3,57 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // http://coffeebreakcodes.com/drag-object-with-mouse-unity3d/ code for getting the mouse to click and dragging object was made using this
-// https://answers.unity.com/questions/373818/how-to-detect-mouse-click-on-a-gameobject.html this was for using a ray to check for mouse collisions
 public class MouseClickAndDrag : MonoBehaviour
 {
     Vector3 screenPoint;
     Vector3 Offset;
 
-    //public GameObject dragObject;
+    bool enableDragging = true;
+    bool holdingObject = false;
 
-    bool overObject;
+    float zPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        zPos = transform.localPosition.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //RaycastHit hit;
 
-        //if(Physics.Raycast(ray, out hit))
-        //{
-        //    if(hit.collider.tag == "WheelPeice")
-        //    {
-        //        overObject = true;
-        //        dragObject = hit.collider.gameObject;
-        //        Debug.Log("found object");
-        //    }
-        //}
-        //else
-        //{
-        //    overObject = false;
-        //    dragObject = null;
-        //}
     }
 
+    public void setDragging(bool _enabled)
+    {
+        enableDragging = _enabled;
+    }
     private void OnMouseDown()
     {
-        //if (overObject && dragObject != null)
-        //{
+        if (enableDragging)
+        {
             screenPoint = Camera.main.WorldToScreenPoint(transform.position);
             Offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-        //}
+            holdingObject = true;
+        }
     }
+
+    private void OnMouseUp()
+    {
+        holdingObject = false;
+    }
+
     private void OnMouseDrag()
     {
-        Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + Offset;
-        transform.position = cursorPosition;
+        if (enableDragging)
+        {
+            Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + Offset;
+            transform.position = cursorPosition;
+            if (transform.localPosition.z != zPos)
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, zPos);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("doin me a collide");
+        if (other.tag == gameObject.tag && !holdingObject)
+        {
+            other.gameObject.GetComponent<MeshRenderer>().material = this.GetComponent<MeshRenderer>().material;
+            gameObject.SetActive(false);
+        }
     }
 
 }
